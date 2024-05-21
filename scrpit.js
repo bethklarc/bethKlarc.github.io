05a758ca84a0510e8669dd652d7349b20c894e84
bethklarc <larakatia86@gmail.com>
2024-05-21 17:01:02 -0600

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -1,5 +1,12 @@
+let signUp = document.getElementById("signUp");
+let signIn = document.getElementById("signIn");
+let nameInput = document.getElementById("nameInput");
+let title = document.getElementById("title");
+let form = document.querySelector("form");
+let emailInput = form.querySelector('input[type="email"]');
+let passwordInput = form.querySelector('input[type="password"]');
+
 signIn.onclick = function () {
-    console.log("Sign In clicked");
     nameInput.style.maxHeight = "0";
     title.innerHTML = "Login";
     signUp.classList.add("disable");
@@ -7,9 +14,20 @@ signIn.onclick = function () {
 }
 
 signUp.onclick = function () {
-    console.log("Sign Up clicked");
     nameInput.style.maxHeight = "60px";
     title.innerHTML = "Registro";
     signUp.classList.remove("disable");
     signIn.classList.add("disable");
 }
+
+// Añadir evento de clic al botón de Login
+signIn.addEventListener("click", function() {
+    // Verificar si los campos están llenos
+    if (emailInput.value !== "" && passwordInput.value !== "") {
+        // Redirigir a la URL especificada
+        window.location.href = "https://ingeniebroslp.my.canva.site/ing-lp";
+    } else {
+        // Mostrar un mensaje de advertencia si los campos no están llenos
+        alert("Por favor, complete todos los campos.");
+    }
+});
