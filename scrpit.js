5072e08abef93c6bea174b00974d5ccb10c75778
bethklarc <larakatia86@gmail.com>
2024-05-21 18:08:04 -0600

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -1,33 +1,33 @@
-let signUp = document.getElementById("signUp");
-let signIn = document.getElementById("signIn");
-let nameInput = document.getElementById("nameInput");
-let title = document.getElementById("title");
-let form = document.querySelector("form");
-let emailInput = form.querySelector('input[type="email"]');
-let passwordInput = form.querySelector('input[type="password"]');
+document.addEventListener('DOMContentLoaded', function () {
+    let signUp = document.getElementById("signUp");
+    let signIn = document.getElementById("signIn");
+    let nameInput = document.getElementById("nameInput");
+    let title = document.getElementById("title");
 
-signIn.onclick = function () {
-    nameInput.style.maxHeight = "0";
-    title.innerHTML = "Login";
-    signUp.classList.add("disable");
-    signIn.classList.remove("disable");
-}
-
-signUp.onclick = function () {
-    nameInput.style.maxHeight = "60px";
-    title.innerHTML = "Registro";
-    signUp.classList.remove("disable");
-    signIn.classList.add("disable");
-}
-
-// Añadir evento de clic al botón de Login
-signIn.addEventListener("click", function() {
-    // Verificar si los campos están llenos
-    if (emailInput.value !== "" && passwordInput.value !== "") {
-        // Redirigir a la URL especificada
-        window.location.href = "https://ingeniebroslp.my.canva.site/ing-lp";
-    } else {
-        // Mostrar un mensaje de advertencia si los campos no están llenos
-        alert("Por favor, complete todos los campos.");
+    signIn.onclick = function () {
+        nameInput.style.maxHeight = "0";
+        title.innerHTML = "Login";
+        signUp.classList.add("disable");
+        signIn.classList.remove("disable");
     }
+
+    signUp.onclick = function () {
+        nameInput.style.maxHeight = "60px";
+        title.innerHTML = "Registro";
+        signUp.classList.remove("disable");
+        signIn.classList.add("disable");
+    }
+
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
+
+        let emailInput = document.getElementById('email').value;
+        let passwordInput = document.getElementById('password').value;
+
+        if (emailInput !== "" && passwordInput !== "") {
+            window.location.href = "https://ingeniebroslp.my.canva.site/ing-lp";
+        } else {
+            alert("Por favor, complete todos los campos.");
+        }
+    });
 });
